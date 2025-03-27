120e2977fbe369d9bd729c945f5b6ee80b73642f
Mantas <9988088@gmail.com>
2025-03-27 21:49:56 +0200

    3

--- Atsiskaitymas2/uzd7.js
+++ Atsiskaitymas2/uzd7.js
@@ -1,15 +1,37 @@
 //C>F
-let duotaCe = 40;
-let apskaiciuotaFa = duotaCe * 1.8 + 32;
-console.log(duotaCe, 'C = ',apskaiciuotaFa, 'F');
+//let duotaCe = 40;
+//let apskaiciuotaFa = duotaCe * 1.8 + 32;
+//console.log(duotaCe, 'C = ',apskaiciuotaFa, 'F');
 
 
 
 for (let i = 0; i < 5; i++) {
     //console.log(i)
-   
-}
- if (i===2) {
-        console.log(*)
+    if (i===0) {
+        console.log('*');
     }
-
+    
+}
+for (let i = 0; i < 5; i++) {
+    
+    if (i===1) {
+        console.log('**');
+    }
+}
+for (let i = 0; i < 5; i++) {
+    
+    if (i===2) {
+        console.log('***');
+    }
+}
+for (let i = 0; i < 5; i++) {
+    
+    if (i===3) {
+        console.log('****');
+    }
+}for (let i = 0; i < 5; i++) {
+    
+    if (i===4) {
+        console.log('*****');
+    }
+}
